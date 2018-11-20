74b518bd05c7ad2b965b8f74ce2f64af8d63cfe0
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 18:43:06 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime? (number)
+ for i in 2..n-1
+    if n % i == 0
+      is_prime = false
+    end
+  end
+  if is_prime = false
+    return false
+  else 
+    return true
+end
