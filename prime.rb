5245696593c8890da12c3d0194607602af0b5b86
Cristian Leiva <cleiva94.cl@gmail.com>
2018-12-03 13:46:16 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(int)
+  is_prime = true
+   return is_prime = false if int < 2
+  (2..int -1).each do |i|
+    if (int % i) == 0
+      is_prime = false
+    end
+  end
+  is_prime
+end
